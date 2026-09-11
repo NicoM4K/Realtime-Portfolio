67b0d0a62998121a59b58285989863deaca85138
NicoM4K <dimarquesnicolas@gmail.com>
2026-09-11 19:41:03 +1200

    11-09-2026 Backup
    Vault backup

---
+++ src/script.js
@@ -0,0 +1,152 @@
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import * as THREE from 'three/webgpu'
+import {positionLocal, time, vec2, checker,uv,float, vec3, sin, vec4} from 'three/tsl'
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.threejs')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Loaders
+const textureLoader = new THREE.TextureLoader()
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 5
+camera.position.y = 4.5
+camera.position.z = 2.5
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 1, 0)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGPURenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(0x111111)
+
+/**
+ * Floor
+ */
+{
+    const texture = textureLoader.load('./floor-color.jpg')
+    texture.colorSpace = THREE.SRGBColorSpace
+    const mesh = new THREE.Mesh(
+        new THREE.PlaneGeometry(10, 10),
+        new THREE.MeshStandardNodeMaterial({ 
+            map: texture,
+            transparent: true})
+    )
+     
+    mesh.rotation.x = - Math.PI * 0.5
+    mesh.receiveShadow = true
+    scene.add(mesh)
+}
+
+/**
+ * Dummy
+ */
+{
+    const geometry = new THREE.TorusKnotGeometry(0.5, 0.24, 128, 32)
+    const material = new THREE.MeshStandardNodeMaterial({
+        color: 0xff0000,
+        roughness: 0.25,
+        metalness: 0.5
+    })
+    material.colorNode = vec3(1,0.4,0.4)
+    material.roughnessNode = float(0)
+    
+    const zoffset = sin(time.add(positionLocal.y.mul(0.3))).mul(0.5).add(0.4)
+    material.positionNode = positionLocal.add(vec3(0,0,zoffset))
+
+    // Animate the pattern
+    const pattern = checker(uv().add(time.mul(0.02)).mul(vec2(40,5)))
+    material.colorNode = vec3(pattern, 0, 0)
+    material.roughnessNode = pattern
+
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.castShadow = true
+    mesh.receiveShadow = true
+    mesh.position.y = 1
+    scene.add(mesh)
+}
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight(0xffffff, 4.5)
+directionalLight.castShadow = true
+directionalLight.position.set(2, 0.75, -1).normalize().multiplyScalar(10)
+directionalLight.shadow.camera.top = 10
+directionalLight.shadow.camera.right = 10
+directionalLight.shadow.camera.bottom = -10
+directionalLight.shadow.camera.left = -10
+directionalLight.shadow.camera.near = 0.01
+directionalLight.shadow.camera.far = 20
+directionalLight.shadow.radius = 10
+directionalLight.shadow.normalBias = 0.1
+scene.add(directionalLight)
+
+const ambientLight = new THREE.AmbientLight(0x859dff, 1)
+scene.add(ambientLight)
+
+/**
+ * Animate
+ */
+const timer = new THREE.Timer()
+timer.connect(document)
+
+const tick = () =>
+{
+    timer.update()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+}
+
+renderer.setAnimationLoop(tick)
+console.log(renderer.backend)
